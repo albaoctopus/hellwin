57b119ffecad925de051ead3fbdd78f8928de7db
Terekhov Anatoly <anatoly.terekhov@gmail.com>
2022-10-30 16:09:08 +0300

    one

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = () => (
     <>
     <nav className="bg-gray-900 border-green-900 ">
       <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
-        <a href="/" className="flex items-center">
+        <a>
           <img
            // src="https://flowbite.com/docs/images/logo.svg"
             //className="mr-3 h-6 sm:h-9"
@@ -27,11 +27,9 @@ export const Navbar = () => (
         <div className="flex items-center">
           <ul className="flex flex-row mt-0 mr-6 space-x-8 text-sm font-medium">
             <li>
-              <a
-                href="/"
-                className="text-gray-900 dark:text-white font-extrabold hover:underline"
-                aria-current="page"
-              >
+              <a>
+            
+              
                
               </a>
             </li>
